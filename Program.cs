92cc91feaef3040a1eb78de3ed3d6fa5b534ccda
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-16 20:08:54 -1200

    welcome page

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using NonStaticLineComparision;
+
+namespace LineComparisionComputationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Computation Problems");
+            //LineDifference Difference = new LineDifference();
+            //Difference.LineDistance();
+
+            // CkeckLineEquality Equality = new CkeckLineEquality();
+            // Equality.LineEquality();
+
+            CompareTwoLines twoLines = new CompareTwoLines();
+            twoLines.CompareLines();
+        }
+}
+}
